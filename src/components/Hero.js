ddf7cbccc42f4ec57232a4711c2d4203c07e052a
Martin Mtandi <martin@shumabmoney.com>
2020-10-10 23:02:42 +0200

    styling hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,18 +5,18 @@ import Watch from '../images/watch.svg'
 function Hero() {
     return (
         <div className="container mx-auto my-12">
-            <div className="grid grid-cols-12 gap-4">
+            <div className="md:grid md:grid-cols-12 md:mx-0 mx-3 gap-4">
                 <div className="text-gray-800 col-span-5">
-                    <h1 className="text-6xl font-bold leading-none">Software <br /> Development <br /> Agency</h1>
+                    <h1 className="md:text-6xl text-5xl font-bold leading-none">Software <br /> Development <br /> Agency</h1>
                     <div className="mt-16 text-2xl">We are a team of professionals focused on the innovation of 
-                        <span className="font-semibold text-indigo-800 ml-2">
+                        <span className="font-semibold text-indigo-800 md:ml-2 md:inline-block block">
                         <Typewriter
-                        options={{
-                            strings: ['web application', 'mobile application', 'software', 'technology', 'virtual reality', 'augmented reality'],
-                            autoStart: true,
-                            loop: true,
-                        }}
-/>
+                            options={{
+                                strings: ['web application', 'mobile application', 'software', 'technology', 'virtual reality', 'augmented reality'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
                         </span>
                         <p>design and development.</p>
                         <button className="bg-red-600 block mt-6 text-base uppercase px-3 py-2 text-gray-100 font-semibold rounded shadow">Let's discuss your project</button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../images/qbright.png'
 
 function Navbar() {
+    const [toggleOpen, setToggleOpen] = useState(false);
+
     return (
     <div>
         <nav className="flex items-center container mx-auto justify-between flex-wrap p-6">
@@ -9,23 +11,23 @@ function Navbar() {
                 <img className="h-24" src={Logo} alt="Logo" />
             </div>
             <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+                <button onClick={() => setToggleOpen(!toggleOpen)} className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
                     <svg className="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto uppercase text-gray-800 font-semibold">
+            <div className={`w-full ${(toggleOpen) ? "block" : "md:block hidden"} flex-grow lg:flex lg:items-center lg:w-auto uppercase text-gray-800 font-semibold`}>
                 <div className="text-sm lg:flex-grow text-right">
-                    <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 mr-6">
+                    <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 md:mr-6">
                         Home
                     </a>
-                    <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 mr-6">
+                    <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 md:mr-6">
                         Services
                     </a>
                     <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0">
                         About
                     </a>
                 </div>
-                <div className="lg:ml-6">
+                <div className="lg:ml-6 text-right md:text-left">
                     <button className="inline-block uppercase px-4 py-2 leading-none border rounded text-white border-red-600 shadow bg-red-600 hover:bg-red-700 mt-4 lg:mt-0">Get Quote</button>
                 </div>
             </div>
